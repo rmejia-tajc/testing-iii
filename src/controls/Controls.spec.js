b1328fcc82df149d2ea7e23162d41af03b9d9068
rmejia-tajc <rmejia.tajc@gmail.com>
2019-03-06 13:43:13 -0700

    add snapshot test to Control.spec.js

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,27 @@
 // Test away!
+
+import React from 'react';
+import { render } from 'react-testing-library';
+import 'react-testing-library/cleanup-after-each';
+import 'jest-dom/extend-expect';
+import renderer from 'react-test-renderer';
+
+import Controls from './Controls.js'
+
+
+
+
+describe('<Controls /> Component', () => {
+
+    it('should match snapshot', () => {
+
+        const tree = renderer.create(<Controls />);
+
+        expect(tree.toJSON()).toMatchSnapshot();
+    });
+
+
+
+
+
+});
