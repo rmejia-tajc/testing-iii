bf3ec5c084d3b8cd941095543545b15488a4a5f1
rmejia-tajc <rmejia.tajc@gmail.com>
2019-03-06 14:01:33 -0700

    add render and defaults test to Controls.spec.js

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -13,6 +13,13 @@ import Controls from './Controls.js'
 
 describe('<Controls /> Component', () => {
 
+
+    it("renders without crashing", () => {
+  
+      const sanityCheck = render(<Controls />);
+    });
+
+
     it('should match snapshot', () => {
 
         const tree = renderer.create(<Controls />);
@@ -21,7 +28,16 @@ describe('<Controls /> Component', () => {
     });
 
 
+    it('should default to unlocked(lock gate) and open(close gate)', () => {
 
+        const { getByText } = render(<Controls />);
+
+        const btnLock = getByText(/lock gate/i);
+        const btnClose= getByText(/close gate/i);
+
+        expect(btnLock).toBeInTheDocument();
+        expect(btnClose).toBeInTheDocument();
+    });
 
 
 });
