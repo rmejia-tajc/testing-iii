95ad940d0474dd3561eaef166bcf0dd92716a2db
rmejia-tajc <rmejia.tajc@gmail.com>
2019-03-06 14:25:04 -0700

    add render, snapshot, and defaults tests to Dashboard.spec.js

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,47 @@
-// Test away
+// Test away!
+
+import React from 'react';
+import { render } from 'react-testing-library';
+import 'react-testing-library/cleanup-after-each';
+import 'jest-dom/extend-expect';
+import renderer from 'react-test-renderer';
+
+import Dashboard from './Dashboard.js'
+
+
+
+
+describe('<Dashboard /> Component', () => {
+
+
+    it("renders without crashing", () => {
+  
+      const sanityCheck = render(<Dashboard />);
+    });
+
+
+    it('should match snapshot', () => {
+
+        const tree = renderer.create(<Dashboard />);
+
+        expect(tree.toJSON()).toMatchSnapshot();
+    });
+
+
+    it('should default to (lock gate)[unlocked] and (close gate)[open]', () => {
+
+        const { getByText } = render(<Dashboard />);
+
+        const unlocked = getByText(/unlocked/i);
+        const open= getByText(/open/i);
+        const btnLock = getByText(/lock gate/i);
+        const btnClose= getByText(/close gate/i);
+
+        expect(unlocked).toBeInTheDocument();
+        expect(open).toBeInTheDocument();
+        expect(btnLock).toBeInTheDocument();
+        expect(btnClose).toBeInTheDocument();
+    });
+
+
+});
